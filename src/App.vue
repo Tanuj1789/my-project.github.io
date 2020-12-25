6e6af4f6d2ec47570fd724c0e7d65a5b90cb7891
Tanuj1789 <58630657+Tanuj1789@users.noreply.github.com>
2020-12-25 17:21:56 +0530

    commiting again

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="header">
+      <ul>
+    <li><router-link to="/">Home </router-link></li>
+    <li><router-link to="/skills">Skills </router-link></li>
+    <li><router-link to="/education">Education </router-link></li>
+    <li><router-link to="/experience">Experience </router-link></li>
+    <li><router-link to="/achievements">Achievements </router-link></li>
+    <li><router-link to="/contact">Contact </router-link></li>
+    </ul>
+
+    </div>
+    <router-view> </router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+
+    }
   }
 }
 </script>
 
-<style>
+<style >
+.header{
+  /* background: black; */
+  padding-left:10px;
+  position: absolute;
+  /* top:0; */
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  background: inherit;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+,
+   Safari 7+ */
+  height:750px;
+  width:100%;
+  /* background-size:cover; */
+  /* background-position: center; */
+}
+body{
+  margin:0px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+ img {
+    filter: grayscale(100%);
+    background: transparent;
+
+}
+img:hover{
+
+  filter: grayscale(0%);
+
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import Experience from './components/experience.vue'
+import Home from './components/home.vue'
+import Education from './components/education.vue'
+import Skills from './components/skills.vue'
+import Achievements from './components/achievements.vue'
+import Contact from './components/contact.vue'
 
-Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const routes=[
+  { path: '/',
+    component:Home
+  },
+  { path: '/skills',
+    component:Skills
+  },
+  { path: '/education',
+    component:Education
+  },
+  { path: '/experience',
+    component:Experience
+  },
+  { path: '/achievements',
+    component:Achievements
+  },
+  {
+    path: '/contact',
+    //for dynamic use
+    // path: '/users/:teamid',
+    component:Contact
+  },
+];
+const router= new VueRouter({
+  routes,
+  mode: 'history'
+})
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+  // renders is a hook to  to various vue files, running the js code present within it
+})
